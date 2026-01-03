a40ce8f3b2bf328373936bee1c59672baa8638a2
HuyNguyen-01 <nguyenhuydtld@gmail.com>
2026-01-03 15:07:19 +0700

    Update UI vao thu muc

---
+++ UI/Lixi_Project_events.py
@@ -0,0 +1,48 @@
+
+def Plus_evnt(event_struct):
+    return
+
+
+def ui_RollOffine(event_struct):
+    return
+
+
+def ui_RollOnline(event_struct):
+    return
+
+
+def ui_BackToPreScreen(event_struct):
+    return
+
+
+def ui_ApplySetting(event_struct):
+    return
+
+
+def ui_HideKeyboard(event_struct):
+    return
+
+
+def SavePreScreen(event_struct):
+    return
+
+
+def SavePreChangeSettingScreen(event_struct):
+    return
+
+
+def RollerScreen4(event_struct):
+    return
+
+
+def RollOffine(event_struct):
+    return
+
+
+def RollOffline(event_struct):
+    return
+
+
+def ui_RollOffline(event_struct):
+    return
+
